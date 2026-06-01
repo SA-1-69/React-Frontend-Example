b0aaeb7d3c7578964edd68a14c5f76acff282a29
Pung pond <nadthapat.phu@gmail.com>
2026-06-01 21:07:13 +0700

    c

--- src/interfaces/Gender.ts
+++
@@ -1,7 +0,0 @@
-export type GenderId = 1 | 2 | 3
-
-export const GENDERS: ReadonlyArray<{ id: GenderId; label: string }> = [
-  { id: 1, label: 'Male' },
-  { id: 2, label: 'Female' },
-  { id: 3, label: 'Other' },
-]
